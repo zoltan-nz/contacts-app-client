30a4f2f204b29a46d74410d1330b81f173e2c643
Zoltan <zdebre@gmail.com>
2015-06-04 00:50:44 +1200

    Add createRecord action to contacts controller

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -5,9 +5,27 @@ export default Ember.Controller.extend({
   isShowingPhoneNumber: false,
 
   actions: {
+
     toggleShowPhoneNumber() {
       this.toggleProperty('isShowingPhoneNumber');
+    },
+
+    addNewContact() {
+
+      var nameFromInput = this.get('name');
+      var phoneFromInput = this.get('phone');
+
+      const newContact = this.store.createRecord('contact', {
+        name: nameFromInput,
+        phone: phoneFromInput
+      });
+
+      newContact.save();
+
+      this.set('name', '');
+      this.set('phone', '');
     }
+
   }
 
 });
