375a754d746c6b03680d21f8e0678bbb7c43a593
Zoltan <zdebre@gmail.com>
2015-07-08 23:15:02 +1200

    Using var instead of let

--- app/components/phone-input.js
+++ app/components/phone-input.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.TextField.extend({
 
   dasherizeDigits: Ember.observer('value', function() {
-    let valueString = this.get('value').replace(/\D/g, '');
+    var valueString = this.get('value').replace(/\D+/g, '');
 
     if (!Ember.isBlank(valueString)) {
       valueString = valueString.match(/\d{1,3}/g).join('-');
